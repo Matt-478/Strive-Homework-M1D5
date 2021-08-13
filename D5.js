9f14c751d2e65b59596295ba24d69694cb977990
Matīss Komļevs <matisskomlevs@gmail.com>
2021-08-13 16:19:11 +0300

    Task 3 finished

--- D5.js
+++ D5.js
@@ -34,6 +34,15 @@ Write a function "crazyDiff" that computes the absolute difference between a giv
 It should return triple their absolute difference if the given number is greater than 19.
 */
 
+const crazyDiff = function (number) {
+  if(number > 19) {
+    tripledValue = (number - 19) * 3
+    return tripledValue
+  } else {
+    return "This number is less than 19, no need to worry."
+  }
+}
+
 /* EXERCISE 4
 Write a function "boundary" which accept an integer n and returns true if n is within 20 and 100 (included) or if it's equal to 400.
 */
