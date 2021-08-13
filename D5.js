e3ec8290c8ccf64e17c845ccc09aed8c1b2d517b
Matīss Komļevs <matisskomlevs@gmail.com>
2021-08-13 16:22:32 +0300

    Task 4 finished

--- D5.js
+++ D5.js
@@ -47,7 +47,15 @@ const crazyDiff = function (number) {
 Write a function "boundary" which accept an integer n and returns true if n is within 20 and 100 (included) or if it's equal to 400.
 */
 
-/* WRITE YOUR CODE HERE */
+const boundary = function(n) {
+  if(n >= 20 && n<= 100) {
+    return true
+  } else if (n === 400) {
+    return true
+  } else {
+    return false
+  }
+}
 
 /* EXERCISE 5
 Write a function "strivify" which accepts a string.
